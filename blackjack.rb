618583039f4f05c4cd01ac56151ec85f3360902e
Asahi20 <asahiyamaguchi1120@gmail.com>
2021-09-15 07:13:32 +0900

    deck,cradの作成

---
+++ blackjack.rb
@@ -0,0 +1,19 @@
+
+require_relative "message"
+require_relative "deck"
+
+class Blackjack
+
+  include Message
+
+  def initialize(dealer, player)
+    @dealer = dealer
+    @player = player
+  end
+
+  def start
+    start_msg
+    @deck = Deck.new
+  end
+
+end
---
+++ player.rb
@@ -0,0 +1,9 @@
+class Player
+
+  INITIAL_MONEY = 10_000
+
+  def initialize
+    @money = INITIAL_MONEY
+
+  end
+end
